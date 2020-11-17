b70b1ed132eca55da02eba6eb365593d5e220834
BB <1036012184@qq.com>
2020-11-17 21:14:37 +0800

    const引用

---
+++ 10_const引用/10_const引用.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int a = 10;
+	const int& x1 = a;
+
+	cout << "&x1 = a = " << x1 <<endl;
+	cout << endl;
+
+	int b = 20;
+	const int& x2 = b;
+	cout << "&x2 = b = " << x2 << endl;
+
+	b = 30;
+	cout << "&x2 = b = " << x2 << endl;
+
+
+}

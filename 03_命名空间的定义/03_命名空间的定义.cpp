37504737132c15c9364334efcb3502250184acc6
BB <1036012184@qq.com>
2020-11-17 17:03:07 +0800

    添加项目文件。

---
+++ 03_命名空间的定义/03_命名空间的定义.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+// _t 代表 typedef 类型
+typedef struct student
+{
+    int num = 39;
+}student_t;
+
+namespace spaceA {
+    // g_ 代表全局变量 global
+    int g_a = 10;
+}
+
+namespace spaceB {
+    int g_b = 20;
+
+    namespace spaceC {
+        struct Dis
+        {
+            int E;
+        };
+    }
+}
+
+int main()
+{
+    using namespace std;
+    using namespace spaceA;
+    using namespace spaceB::spaceC;
+    struct Dis x1;
+    x1.E = 10;
+    student_t x2;
+    x2.num = 39;
+    cout << "hello" << endl;
+    cout << "stu_t.num = " << x2.num << endl;
+    cout << "g_a = " << g_a << endl;
+    cout << "E = " << x1.E << endl;
+}
---
+++ 命名空间/02_命名空间.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+// 方式二：
+#if 0
+// 声明命名空间中的一个变量
+using std::cout;
+using std::endl;
+using std::cin;
+#endif // 0
+
+// 方式三：
+using namespace std;
+
+int main(void)
+{
+    int a = 0;
+// 方式一：
+#if 0
+    std::cout << "Hello World!\n";
+#endif
+
+    cout << "Enter a char:";
+
+    cin >> a;
+
+    cout << "a=" << a << endl;
+
+    return 0;
+}
+

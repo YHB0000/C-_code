c2cbb6ab3af83f26d28d4d7908153601efc0f3b6
BB <1036012184@qq.com>
2020-11-18 20:28:14 +0800

    默认参数和占位参数

---
+++ day02/02_默认参数和占位参数/02_默认参数和占位参数.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int area(int w = 9, int h = 8, int l = 3)
+{
+    return w * h * l;
+}
+
+void fun(int x, int = 0)
+{
+    cout << "x = " << x << " with 占位符 " << endl;
+}
+
+int main()
+{
+    int v = 1;
+    cout << "area = " << area() << endl;
+    cout << "-----------------------" << endl;
+    fun(v);
+}

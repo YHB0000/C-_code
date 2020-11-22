42990d5b622b1ff0c0fd34a2302fa8b526a67bec
BB <1036012184@qq.com>
2020-11-22 15:24:58 +0800

    类的基本概念

---
+++ day02/05_类的基本概念/05_类的基本概念.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+	char name[64];
+	char color[64];
+
+	void PrintAll()
+	{
+		cout << "name: " << name << endl;
+		cout << "color: " << color << endl;
+	}
+
+	void write()
+	{
+		cout << "write something ..." << endl;
+	}
+
+	void run()
+	{
+		cout << "running ..." << endl;
+	}
+
+private:
+
+};
+
+int main()
+{
+	Animal dog;
+	strcpy_s(dog.name, "dog");
+	strcpy_s(dog.color, "black");
+	dog.PrintAll();
+	dog.run();
+
+	cout << "--------------" << endl;
+	Animal sheep;
+	strcpy_s(sheep.name, "sheep");
+	strcpy_s(sheep.color, "white");
+	sheep.PrintAll();
+	sheep.write();
+}

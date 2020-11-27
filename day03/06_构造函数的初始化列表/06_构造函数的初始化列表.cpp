c59a05575de4233f385af59be3f9436655357696
BB <1036012184@qq.com>
2020-11-27 14:45:28 +0800

    构造函数的初始化列表（常量补充）

--- day03/06_构造函数的初始化列表/06_构造函数的初始化列表.cpp
+++ day03/06_构造函数的初始化列表/06_构造函数的初始化列表.cpp
@@ -31,24 +31,17 @@ private:
 class B
 {
 public:
-	B()
-	{
-		m_id = 0;
-	}
-	B(int id)
-	{
-		m_id = id;
-	}
 	/*
 		构造对象成员的顺序与初始化列表的顺序无关
 		而与成员对象的定义顺序有关
+		【注】类中的常量声明后，需要在初始化列表中进行初始化
 	*/
-	B(A &a, A &b,int id) : m_a(a), m_b(b)
+	B(A &a, A &b,int id, int m) : m_a(a), m_b(b), m_m(m)
 	{
 		cout << "---- inital B " << id << "----" << endl;
 		m_id = id;
 	}
-	B(int a, int b, int id) : m_a(a), m_b(b)
+	B(int a, int b, int id, int m) : m_a(a), m_b(b), m_m(m)
 	{
 		cout << "---- inital B " << id << "----" << endl;
 		m_id = id;
@@ -56,6 +49,7 @@ public:
 	void showB()
 	{
 		cout <<  "id_B = " << m_id << endl;
+		cout << "m_B = " << m_m << endl;
 		m_a.showA();
 		m_b.showA();
 	}
@@ -65,20 +59,22 @@ public:
 	}
 
 private:
+	// 在声明定义中进行赋值是无效的（语法错误的）
 	int m_id;
 	// 定义顺序！
 	A m_a;
 	A m_b;
+	const int m_m;
 };
 
 void fun()
 {
-	B c1(1, 2, 3);
+	B c1(1, 2, 3, 999);
 	c1.showB();
 	cout << "-------------------------------" << endl;
 	A d1(10);
 	A d2(20);
-	B c2(d1, d2, 30);
+	B c2(d1, d2, 30 , 9999);
 	c2.showB();
 }
 

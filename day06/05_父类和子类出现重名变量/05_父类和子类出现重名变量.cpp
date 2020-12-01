e57a00d855061715c069308e1604a971e5313ffb
BB <1036012184@qq.com>
2020-12-01 20:51:41 +0800

    父类和子类出现重名变量

---
+++ day06/05_父类和子类出现重名变量/05_父类和子类出现重名变量.cpp
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+class Parent
+{
+public:
+	Parent()
+	{
+		cout << "------ init() ------" << endl;
+		this->x = 0;
+	}
+	Parent(int x)
+	{
+		cout << "------ init(int x) ------" << endl;
+		this->x = x;
+	}
+	void show()
+	{
+		cout << "x = " << this->x << endl;
+	}
+	~Parent()
+	{
+		cout << "------ bey ------" << endl;
+	}
+	int x;
+};
+
+class Child :public Parent
+{
+public:
+	Child()
+	{
+		cout << "------ init() ------" << endl;
+		this->x = 0;
+	}
+	Child(int p_x, int c_x) :Parent(p_x)
+	{
+		cout << "------ init(int p_x, int c_x) ------" << endl;
+		this->x = c_x;
+	}
+	void show()
+	{
+		cout << "p_x = " << Parent::x << endl;
+		cout << "c_x = " << this->x << endl;
+	}
+	~Child()
+	{
+		cout << "------ bey ------" << endl;
+	}
+	int x;
+};
+
+int main()
+{
+	Child c(10, 20);
+	c.show();
+	return 0;
+}
